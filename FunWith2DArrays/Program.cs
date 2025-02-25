1cdb7d4167022f145216394b8687e780f2ed1efb
howiebread <49467548+howiebread@users.noreply.github.com>
2025-02-25 18:03:09 -0500

    Define allowed characters program will generate as input at random.

--- FunWith2DArrays/Program.cs
+++ FunWith2DArrays/Program.cs
@@ -13,30 +13,15 @@ class Program
         //Declaring 2D Array user selected.
         char[,] array2D = new char [rows, columns];
         
-        // Putting info in the array the user inputs.
-        Console.WriteLine("Enter the elements of the array:  Can use a numbers 0-9 or special characters! ");
+        // Define allowed characters (digits and some special characters).
+        string allowedCharacters = "0123456789!@#$%^&*";
+        
+        
         for (int row = 0; row < rows; row++)
         {
             for (int column = 0; column < columns; column++)
             {
-                string userInput;
-                // Loop until only one character is entered.
-                while (true)
-                {
-                    Console.Write($"Element at [{row}, {column}]: ");
-                    userInput = Console.ReadLine();
-                    
-                    // Condition to break the loop if the user selects one character.
-                    if (!string.IsNullOrEmpty(userInput) && userInput.Length == 1)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input.  Please enter exactly one character!");
-                    }
-                }
-                array2D[row, column] = userInput[0];
+                
             }
         }
         // Outputting the 2D Array
