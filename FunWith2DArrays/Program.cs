f90df13caed8e46f524c4f861a0a03a2ce3ced9d
howiebread <49467548+howiebread@users.noreply.github.com>
2025-02-25 14:08:56 -0500

    Changed the array to char and created a check to make sure user enters only one character.

--- FunWith2DArrays/Program.cs
+++ FunWith2DArrays/Program.cs
@@ -11,16 +11,32 @@ class Program
         int columns = int.Parse(Console.ReadLine());
         
         //Declaring 2D Array user selected.
-        int[,] array2D = new int [rows, columns];
+        char[,] array2D = new char [rows, columns];
         
         // Putting info in the array the user inputs.
-        Console.WriteLine("Enter the elements of the array:  Must be a number! ");
+        Console.WriteLine("Enter the elements of the array:  Can use a numbers 0-9 or special characters! ");
         for (int row = 0; row < rows; row++)
         {
             for (int column = 0; column < columns; column++)
             {
-                Console.Write($"Element at [{row}, {column}]: ");
-                array2D[row, column] = int.Parse(Console.ReadLine());
+                string userInput;
+                // Loop until only one character is entered.
+                while (true)
+                {
+                    Console.Write($"Element at [{row}, {column}]: ");
+                    userInput = Console.ReadLine();
+                    
+                    // Condition to break the loop if the user selects one character.
+                    if (!string.IsNullOrEmpty(userInput) && userInput.Length == 1)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input.  Please enter exactly one character!");
+                    }
+                }
+                array2D[row, column] = userInput[0];
             }
         }
         // Outputting the 2D Array
