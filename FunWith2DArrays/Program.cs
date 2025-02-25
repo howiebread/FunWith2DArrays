f623903ba0e63bce0086fd2c0c8ddb3b9a333dff
howiebread <49467548+howiebread@users.noreply.github.com>
2025-02-24 19:58:00 -0500

    Set up getting dimensions of the Array from the user.

--- FunWith2DArrays/Program.cs
+++ FunWith2DArrays/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Enter the number of rows: ");
+        int rows = int.Parse(Console.ReadLine());
         
-    }
+        Console.WriteLine("Enter the number of columns: ");
+    }   int columns = int.Parse(Console.ReadLine());
+    
 }
