e471ded7a079e615839b495a667fcc4cb7558b46
howiebread <49467548+howiebread@users.noreply.github.com>
2025-02-24 23:31:10 -0500

    Adding info the user inputs into the Array.

--- FunWith2DArrays/Program.cs
+++ FunWith2DArrays/Program.cs
@@ -13,6 +13,16 @@ class Program
         //Declaring 2D Array user selected.
         int[,] array2D = new int [rows, columns];
         
+        // Putting info in the array the user inputs.
+        Console.WriteLine("Enter the elements of the array: ");
+        for (int row = 0; row < rows; row++)
+        {
+            for (int column = 0; column < columns; column++)
+            {
+                Console.Write($"Element at [{row}, {column}]: ");
+                array2D[row, column] = int.Parse(Console.ReadLine());
+            }
+        }
     }
 
 }   
