86f522b21583d13a2bdbb9434d67d78a72b3291a
howiebread <49467548+howiebread@users.noreply.github.com>
2025-02-27 02:11:12 -0500

    Removed extra line spaces.

--- FunWith2DArrays/Program.cs
+++ FunWith2DArrays/Program.cs
@@ -9,16 +9,13 @@ class Program
     {
         Console.WriteLine("Enter the number of rows: ");
         rows = int.Parse(Console.ReadLine());
-
         Console.WriteLine("Enter the number of columns: ");
         columns = int.Parse(Console.ReadLine());
-        
         // Print the grid
         for (int row = 0; row < rows; row++)
         {
             // Print the top border.
             PrintBorder(columns);
-            
             // Print the row contents.
             for (int column = 0; column < columns; column++)
             {
@@ -26,7 +23,6 @@ class Program
             }
             Console.WriteLine("|");
         }
-        
         // Print the bottom border.
         PrintBorder(columns);
     }
