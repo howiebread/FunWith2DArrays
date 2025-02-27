acf4ccb0b88692c5442065c501d5d4eeffb8d7d2
howiebread <49467548+howiebread@users.noreply.github.com>
2025-02-27 02:03:49 -0500

    Outputted the Grid.

--- FunWith2DArrays/Program.cs
+++ FunWith2DArrays/Program.cs
@@ -12,11 +12,27 @@ class Program
 
         Console.WriteLine("Enter the number of columns: ");
         columns = int.Parse(Console.ReadLine());
-
+        
+        // Print the grid
+        for (int row = 0; row < rows; row++)
+        {
+            // Print the top border.
+            PrintBorder(columns);
+            
+            // Print the row contents.
+            for (int column = 0; column < columns; column++)
+            {
+                Console.Write($"|{row},{column}");
+            }
+            Console.WriteLine("|");
+        }
+        
+        // Print the bottom border.
+        PrintBorder(columns);
 
     }
 // Prints a horizontal border line. +---+---+---+
-    void PrintBorder(int columns)
+    static void PrintBorder(int columns)
     {
         for (int column = 0; column < columns; column++)
         {
